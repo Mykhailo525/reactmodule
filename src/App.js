ed43eccbd18664cb4506e9b070f2224b7817dd6c
Mykhailo525 <mtrindak@gmail.com>
2022-09-18 20:02:58 +0300

    First commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 
 import './App.css';
+
+import Simpsons from "./components/Simpsons/Simpsons";
+
 import TheSimpsons from "./components/TheSimpsons/TheSimpsons";
 
 
 function App() {
   return (
     <div>
+
       {/*{<Simpsons/>}*/}
 
       {<TheSimpsons/>}
+
     </div>
   );
 }
